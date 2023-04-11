b6ed2833cd24e51208620c2098093a60392b87a2
SleeplessChallenger <sausernsunny@gmail.com>
2023-04-11 11:24:57 +0300

    Revamped InitialChecks for OpenFeign + Eureka

--- initialchecks/src/main/java/com/initialchecks/process/InitialChecksApplication.java
+++ initialchecks/src/main/java/com/initialchecks/process/InitialChecksApplication.java
@@ -2,7 +2,10 @@ package com.initialchecks.process;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InitialChecksApplication {
     public static void main(String[] args) {
--- initialchecks/src/main/java/com/initialchecks/process/controller/InitialChecksController.java
+++ initialchecks/src/main/java/com/initialchecks/process/controller/InitialChecksController.java
@@ -1,16 +1,15 @@
 package com.initialchecks.process.controller;
 
+import com.feign.clients.initialchecks.ApplicationRequest;
 import com.initialchecks.process.dto.ApplicationCheck;
 import com.initialchecks.process.service.InitialChecksService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -22,10 +21,14 @@ public class InitialChecksController {
 
     private final InitialChecksService initialChecksService;
 
-    @PostMapping(path = "{applicantId}/{depositId}")
-    public ResponseEntity<String> checkApplication(@PathVariable @NotBlank String applicantId,
-                                                   @PathVariable @NotBlank String depositId) {
-        log.info("Started checking application with userId = {} and depositId = {}", applicantId, depositId);
+    @PostMapping(path = "{requestId}")
+    public ResponseEntity<String> checkApplication(@PathVariable @NotBlank String requestId,
+                                                   @RequestBody @Valid ApplicationRequest applicationRequest) {
+        final String applicantId = applicationRequest.getApplicantId();
+        final String depositId = applicationRequest.getDepositId();
+        log.info("Request = {}. Started checking application with userId = {} and depositId = {}",
+                requestId, applicantId, depositId);
+        // TODO: persist data with requestId that we start checking
 
         // Accept request from Tomcat and give to Java Executor. Don't wait and give response
         initialChecksService.checkApplication(ApplicationCheck.builder()
